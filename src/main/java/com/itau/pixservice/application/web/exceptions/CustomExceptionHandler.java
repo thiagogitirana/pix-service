7c1d1b653544c0950ea8805c131b02fbf3c08761
Thiago Pereira Gitirana <45773328+thiagogitirana@users.noreply.github.com>
2022-06-16 02:44:45 -0300

    Create exceptionhandler

---
+++ src/main/java/com/itau/pixservice/application/web/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.itau.pixservice.application.web.exceptions;
+
+import com.itau.pixservice.application.web.dto.response.PixErrorDto;
+import com.itau.pixservice.domain.exceptions.InvalidKeyException;
+import com.itau.pixservice.domain.exceptions.InvalidParamiterException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(value = {InvalidKeyException.class, InvalidParamiterException.class})
+    public ResponseEntity<PixErrorDto> validationErros(Exception exception) {
+
+        PixErrorDto error = new PixErrorDto();
+        error.setMessage(exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+    }
+}
